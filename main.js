6474adce1e4e65daceca9ac87db036960a6dfe90
shivani2893 <shivi.0928@gmail.com>
2017-02-28 22:56:31 +0530

    final file

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
+Vue.component('deletenote', {
+
+    props : ['title'],
+
+    data: function () {
+        return{
+
+            title: 'notes',
+            isvisible: true
+        }
+
+    },
+
+    template:
+        '<span>\
+      <li class="list-group-item" v-show="isvisible">\
+          {{ title }}\
+          <button type="button" @click="isvisible= false">X</button>\
+      </li>\</span>'
+  });
+
 
 new Vue ({
 
-    el: '#root'
-})
+    el: '#root',
+
+    data: {
+
+        notes: ['Appointment with doctor.',
+            'Pick up dry cleaning.',
+            'Submit assignment.'
+        ]
+
+
+    },
+
+
+    methods: {
+
+        addnote: function () {
+            //function adds new notes to the list
+            this.notes.push(this.newnote);
+
+            this.newnote = '';
+        }
+
+        }
+
+
+
+});
